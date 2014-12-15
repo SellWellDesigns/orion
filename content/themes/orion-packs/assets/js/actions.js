23ca4f68eb1438e52732339dee1d14cdadf55f42
Conar Welsh <conar@sellwell.io>
2014-12-15 14:23:11 -0700

    update styles

--- content/themes/orion-packs/assets/js/actions.js
+++ content/themes/orion-packs/assets/js/actions.js
@@ -1,3 +1,32 @@
 (function(){
 	'use strict';
+
+	/**
+	 * Tabs
+	 */
+	$(document).on('click', '[data-tab]', function(){
+		var tab = $(this).data('tab');
+		
+		$('.tab').removeClass('active');
+		$('#' + tab).addClass('active');
+	});
+
+	/**
+	 * AJAX Pagination
+	 */
+	$(document).on('click', '.pagination a', function(e){
+		e.preventDefault();
+
+		var
+			$self = $(this),
+			href = $self.attr('href') || '/'
+		;
+
+		$.get(href, function(data){
+			$('#blogRoll').html( $('#blogRoll', data).html() );
+		});
+
+		return false;
+	});
+
 }());
