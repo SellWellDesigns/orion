ec63b91acfd720e7d3a93bea70964c2493cd0cbd
Conar Welsh <conar@sellwelldesigns.com>
2014-12-16 14:02:06 -0700

    add content

--- content/themes/orion-packs/assets/js/actions.js
+++ content/themes/orion-packs/assets/js/actions.js
@@ -7,26 +7,8 @@
 	$(document).on('click', '[data-tab]', function(){
 		var tab = $(this).data('tab');
 		
-		$('.tab').removeClass('active');
-		$('#' + tab).addClass('active');
-	});
-
-	/**
-	 * AJAX Pagination
-	 */
-	$(document).on('click', '.pagination a', function(e){
-		e.preventDefault();
-
-		var
-			$self = $(this),
-			href = $self.attr('href') || '/'
-		;
-
-		$.get(href, function(data){
-			$('#blogRoll').html( $('#blogRoll', data).html() );
-		});
-
-		return false;
+		$('[data-tab]').add('.tab').removeClass('active');
+		$('[data-tab="' + tab + '"]').add('#' + tab).addClass('active');
 	});
 
 }());
